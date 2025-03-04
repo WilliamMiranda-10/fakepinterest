d96158a78eeec19f770045b650c0d58775a4e265
WILLIAM MIRANDA AMORIM <willianmiranda127@gmail.com>
2025-03-03 20:11:03 -0400

    ajuste login

--- fakePinterest/routes.py
+++ fakePinterest/routes.py
@@ -12,7 +12,7 @@ def homepage():
     form_login = FormLogin()
     if form_login.validate_on_submit():
         usuario = Usuario.query.filter_by(email=form_login.email.data).first()
-        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+        if usuario and bcrypt.check_password_hash(usuario.senha.encode('utf-8'), form_login.senha.data):
             login_user(usuario)
             return redirect(url_for('perfil', id_usuario=usuario.id))
     return render_template('homepage.html', form=form_login)
@@ -22,7 +22,7 @@ def homepage():
 def criar_conta():
     formcriar_conta = FormCriarConta()
     if formcriar_conta.validate_on_submit():
-        senha_cript = bcrypt.generate_password_hash(formcriar_conta.senha.data)
+        senha_cript = bcrypt.generate_password_hash(formcriar_conta.senha.data).decode('utf-8')
         usuario = Usuario(username=formcriar_conta.username.data, email=formcriar_conta.email.data, senha=senha_cript)
         database.session.add(usuario)
         database.session.commit()
